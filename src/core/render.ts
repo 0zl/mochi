72b69b30d113b2de0df82086d144bf0c49b0f9ea
0zl <zwiefk0@gmail.com>
2024-06-05 21:36:16 +0700

    scss

--- src/core/render.ts
+++ src/core/render.ts
@@ -1,7 +1,10 @@
 import pug from 'pug'
+import * as sass from 'sass'
 
 class MochiRender {
-    async get(subPath: string, data: Record<string, any> = {}) {
+    private StyleCache: Map<number | bigint, string> = new Map()
+
+    async html(subPath: string, data: Record<string, any> = {}) {
         const path = Mochi.getDir(`src/views/${subPath}`)
         const file = Bun.file(path)
         
@@ -15,6 +18,27 @@ class MochiRender {
             filename: path,
             ...data
         })
+    }
+
+    async style(name: string) {
+        const path = Mochi.getDir(`src/scss/${name}.scss`)
+        const file = Bun.file(path)
+
+        if ( !await file.exists() ) {
+            throw new Error(`Style ${path} not found`)
+        }
+
+        const text = await file.text()
+        const hash = Bun.hash(text)
+        
+        if ( this.StyleCache.has(hash) ) {
+            return this.StyleCache.get(hash)!
+        }
+
+        const compiled = sass.compile(path)
+        this.StyleCache.set(hash, compiled.css)
+        
+        return compiled.css
     }
 }
 
--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,6 +1,16 @@
 import { Elysia } from 'elysia'
 
 export default new Elysia()
+    .get('/style/:name', async (req) => {
+        try {
+            const style = await Mochi.Render.style(req.params.name)
+            req.set.headers['Content-Type'] = 'text/css'
+            return style
+        } catch (error) {
+            console.error(error)
+            req.set.status = 400
+        }
+    })
     .all('/', () => {
-        return Mochi.Render.get('index.pug', {})
+        return Mochi.Render.html('index.pug', {})
     })
