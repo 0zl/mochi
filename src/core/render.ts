2b6207258d4829f0232a8165fb713ea2f7644ac5
0zl <zwiefk0@gmail.com>
2024-06-05 19:13:18 +0700

    removed render caching

--- src/core/render.ts
+++ src/core/render.ts
@@ -1,19 +1,8 @@
 import pug from 'pug'
 
 class MochiRender {
-    private Cache: Map<string, string> = new Map()
-
     async get(subPath: string, data: Record<string, any> = {}) {
         const path = Mochi.getDir(`src/views/${subPath}`)
-
-        if ( this.Cache.has(path) ) {
-            const cachedText = this.Cache.get(path)!
-            return pug.render(cachedText, {
-                filename: path,
-                ...data
-            })
-        }
-
         const file = Bun.file(path)
         
         if ( !await file.exists() ) {
@@ -21,7 +10,6 @@ class MochiRender {
         }
 
         const text = await file.text()
-        this.Cache.set(path, text)
 
         return pug.render(text, {
             filename: path,
